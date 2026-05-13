9760f729aaf0852cbdb01a17ce6c136ef0eb5629
Carlos-Emmanuel-ISC6 <carlosglezalv2004@gmail,com>
2026-05-12 21:11:01 -0600

    NORMALIZAR TEXTOS

--- app/src/main/java/com/example/sitema/AdministradorActivity.java
+++ app/src/main/java/com/example/sitema/AdministradorActivity.java
@@ -43,7 +43,7 @@ public class AdministradorActivity extends AppCompatActivity {
             @Override
             public void onPageSelected(int position) {
                 super.onPageSelected(position);
-                toolbar.setTitle("Sección: " + tabTitles[position]);
+                toolbar.setTitle(tabTitles[position]);
             }
         });
     }
